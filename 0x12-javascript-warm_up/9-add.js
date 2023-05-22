2f8da1cd2d982658990f50d3a596446da3de5f47
Chacha-A-Chacha <calex2607@gmail.com>
2023-05-22 18:04:37 +0300

    Update 9-add.js
    number();

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,16 @@
 #!/usr/bin/node
+// function add (a, b) {
+//   return a + b;
+// }
+
+// console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+const a = Number(process.argv[2]);
+const b = parseInt(process.argv[3]);
+if (isNaN(a , b)) {
+    console.log('Please enter a number');
+}
+
 function add (a, b) {
   return a + b;
 }
-
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+console.log(add(a, b));
