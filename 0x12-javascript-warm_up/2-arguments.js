bcf0e3fb4cfcbc453dec59a2db21744aa04d034d
Chacha-A-Chacha <calex2607@gmail.com>
2023-05-22 15:50:28 +0300

    modified
    process.argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 import { argv } from 'node:process';
-//const { argv } = require('node:process');
+/* CommonJs module 
+ * CJS: Primarily used in server-side environments, such as Node.js. 
+ *  It is not natively supported in browsers without bundlers or transpilers.
+ * ESM: Supported in modern browsers and can be used without transpilation or bundling, 
+ *  allowing for native module loading
+ */
+//const { argv } = require('node:process'); 
 //const count = process.argv.length;
 const count = argv.length;
 console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
+const argv = require('node:process');
+if (argv[2] === undefined) {
+    console.log('No argument')
+
+}
+else {
+    console.log(argv[2]);
+}
+
+
 console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
