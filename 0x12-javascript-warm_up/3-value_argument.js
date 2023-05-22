7cffaf7a4879510a8b7a21a5f26143af41b1ea92
Chacha-A-Chacha <calex2607@gmail.com>
2023-05-22 16:28:09 +0300

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-const argv = process.argv[2];
-if (argv === undefined) {
-    console.log('No argument')
 
-}
-else {
-    console.log(argv);
-}
+// const argv = process.argv[2];
+// if (argv === undefined) {
+//     console.log('No argument')
+
+// }
+// else {
+//     console.log(argv);
+// }
 
 
 console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
