ce51a37afb7b8ceffb2015d435cf3783ea2b95f6
Chacha-A-Chacha <calex2607@gmail.com>
2023-05-22 13:04:36 +0300

    Update 1-multi_languages.js
    commented out the first code

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
-
+/*
+ *console.log('C is fun\nPython is cool\nJavaScript is amazing');
+ */
 const var1 = "C is fun";
 const var2 = "Python is cool";
 const var3 = "JavaScript is amazing";
-
 
 console.log(`${var1}\n${var2}\n${var3}`);
