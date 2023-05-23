7234f8cff5ee89f114fdad4657f6320b03fcd267
Chacha-A-Chacha <calex2607@gmail.com>
2023-05-23 10:09:39 +0300

    Update 101-main.js

--- 0x12-javascript-warm_up/101-main.js
+++ 0x12-javascript-warm_up/101-main.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node 
-
+//CommonJS Syntax
 const callMeMoby = require('./101-call_me_moby').callMeMoby
 callMeMoby(3, function() {
     console.log('Common JS is fun')
 })
 
-import { call_me_moby } from './101-call_me_moby';
+const { call_me_moby }  = require('./101-call_me_moby');
 call_me_moby(3, function() {
     console.log('C is fun ')
