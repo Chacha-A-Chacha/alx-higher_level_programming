cd2938f64ea31d2787be8709db9934c197ad8396
Chacha-A-Chacha <calex2607@gmail.com>
2023-05-30 16:11:45 +0300

    modified:   0x13-javascript_objects_scopes_closures/101-sorted.js 	modified:   0x13-javascript_objects_scopes_closures/102-concat.js 	modified:   0x13-javascript_objects_scopes_closures/8-esrever.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -8,3 +8,20 @@ for (const N in dict) {
     Dictn[dict[N]].push(N);
 }
 console.log(Dictn);
+
+
+/*
+ * imports a dictionary of occurences by user ID
+ * computes a dictionary of user IDs bu occurence
+ * 
+ */
+
+const dict =require('./101-data').dict;
+const newDict = {};
+
+Object.keys(dict).map(() => (key, index) {
+    if (newDict[dict[key]] === undefined) {
+        newDict[dict[key]] = [];
+    }
+    newDict[dict[key]].push(key)
+});
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,18 @@
 #!/usr/bin/node
+
+/* 
+ * concats 2 files 
+ */
+
 const fs = require('fs');
 const a = fs.readFileSync(process.argv[2], 'utf8');
 const b = fs.readFileSync(process.argv[3], 'utf8');
 fs.writeFileSync(process.argv[4], a + b);
+
+
+
+const finalFile = require('fs');
+const firstFile = fs.readFileSync(process.argc[2]).toString();
+const secondFile = fs.readFileSync(process.argv[3]).toString();
+
+fs.writeFileSync(process.argv[4], (firstFile + secondFile));
